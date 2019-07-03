687289293755fdeb0e3c292c85c5ed082d5dc6d9
pxh <peixinhua@unisound.com>
2019-07-03 11:50:31 +0800

    show tableview content and navigation title

--- ScrollModifyNavi/ViewController.swift
+++ ScrollModifyNavi/ViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
+/// 滚动修改 导航栏
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.deploySubviews()
+        self.navigationItem.title = "test"
     }
 
+    
+    private func deploySubviews(){
+        tableview.delegate = self
+        tableview.dataSource = self
+        tableview.tableFooterView = UIView.init()
+    }
 
+}
+
+// MARK: - UITableViewDelegate,UITableViewDataSource
+extension ViewController: UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellID = "cellID"
+        var cell = tableview.dequeueReusableCell(withIdentifier: cellID)
+        if cell == nil {
+            cell = UITableViewCell.init(style: .subtitle, reuseIdentifier: cellID)
+        }
+        cell?.textLabel?.text = "\(indexPath.row)"
+        cell?.detailTextLabel?.text = "test"
+        return cell!
+    }
+    
+    
 }
 
